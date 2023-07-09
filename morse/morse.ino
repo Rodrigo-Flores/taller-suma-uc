c3a77d73c58f8e396593d55663a0949599340259
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2023-07-09 00:59:58 -0400

    Morse funcionando (la cadena debe terminar con /)

---
+++ morse/morse.ino
@@ -0,0 +1,104 @@
+#include <Arduino.h>
+#include <string.h>
+
+char characters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                     'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3',
+                     '4', '5', '6', '7', '8', '9', ' '};
+
+const char* morseCodes[] = {".-",   "-...", "-.-.", "-..",  ".",   "..-.", "--.",  "....", "..",
+                            ".---", "-.-", ".-..", "--",   "-.",  "---",  ".--.", "--.-", ".-.",
+                            "...",  "-",   "..-",  "...-", ".--", "-..-", "-.--", "--..", "-----",
+                            ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "/"};
+
+String cifrar(String mensaje) {
+    String mensaje_cifrado = "";
+
+    for (int i = 0; i < mensaje.length(); i++) {
+        char caracter = toupper(mensaje.charAt(i));
+
+        if (caracter == ' ') {
+            mensaje_cifrado += "/ ";
+        } else {
+            for (int j = 0; j < sizeof(characters); j++) {
+                if (characters[j] == caracter) {
+                    mensaje_cifrado += morseCodes[j];
+                    mensaje_cifrado += " ";
+                    break;
+                }
+            }
+        }
+    }
+
+    mensaje_cifrado += "/";
+    return mensaje_cifrado;
+}
+
+String descifrar(String mensaje_cifrado) {
+    String mensaje_descifrado = "";
+    String morse = "";
+
+    for (int i = 0; i < mensaje_cifrado.length(); i++) {
+        char caracter = mensaje_cifrado.charAt(i);
+
+        if (caracter != ' ') {
+            morse += caracter;
+        } else {
+            if (morse == "/") {
+                mensaje_descifrado += ' ';  // Agregar espacio entre palabras
+            } else {
+                bool encontrado = false;
+                for (int j = 0; j < sizeof(morseCodes); j++) {
+                    if (strcmp(morseCodes[j], morse.c_str()) == 0) {
+                        mensaje_descifrado += characters[j];
+                        encontrado = true;
+                        break;
+                    }
+                }
+                if (!encontrado) {
+                    mensaje_descifrado += 'e';  // Caracter desconocido
+                }
+            }
+            morse = "";
+        }
+    }
+
+    return mensaje_descifrado;
+}
+
+void setup() {
+    Serial.begin(9600);
+
+    String mensaje;
+    int opcion;
+
+    Serial.println("Ingrese el mensaje: ");
+    while (!Serial.available()) {
+        // Esperar hasta que se ingrese el mensaje en el monitor serial
+    }
+    mensaje = Serial.readString();
+
+    Serial.println("Elija una opción: ");
+    Serial.println("1. Cifrar mensaje en código Morse");
+    Serial.println("2. Descifrar mensaje en código Morse");
+
+    while (!Serial.available()) {
+        // Esperar hasta que se ingrese la opción en el monitor serial
+    }
+    opcion = Serial.parseInt();
+
+    String mensaje_transformado;
+    if (opcion == 1) {
+        mensaje_transformado = cifrar(mensaje);
+        Serial.println("Mensaje cifrado en código Morse: " + mensaje_transformado);
+    } else if (opcion == 2) {
+        mensaje_transformado = descifrar(mensaje);
+        Serial.println("Mensaje descifrado en código Morse: " + mensaje_transformado);
+    } else {
+        Serial.println("Opción no válida.");
+    }
+}
+
+void loop() {
+    // El código principal se ejecuta en el loop solo una vez
+}
