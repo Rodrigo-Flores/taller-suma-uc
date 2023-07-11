3114f6eb275703354290f23cc595cca57849806e
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2023-07-11 00:57:09 -0400

    receptor y transmisor listo para probar

--- cesar/enviar_cesar.ino
+++ cesar/enviar_cesar.ino
@@ -3,19 +3,70 @@
 #include <RF24.h>
 #include <Arduino.h>
 
+// Función para cifrar el mensaje usando el cifrado de César
+String cifrar(String mensaje, int clave)
+{
+    String mensaje_cifrado = "";
+
+    for (int i = 0; i < mensaje.length(); i++)
+    {
+        if (isAlpha(mensaje.charAt(i)))
+        {
+            char caracter = isUpperCase(mensaje.charAt(i)) ? 'A' : 'a';
+            mensaje_cifrado += (char)(((mensaje.charAt(i) - caracter + clave) % 26) + caracter);
+        }
+        else
+        {
+            mensaje_cifrado += mensaje.charAt(i);
+        }
+    }
+
+    return mensaje_cifrado;
+}
+
+// Función para descifrar el mensaje usando el cifrado de César
+String descifrar(String mensaje_cifrado, int clave)
+{
+    return cifrar(mensaje_cifrado, 26 - clave);
+}
+
 RF24 radio(7, 8); // CE, CSN
 
 const byte address[6] = "00001";
 
-void setup() {
-  radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+void setup()
+{
+    Serial.begin(9600);
+    radio.begin();
+    radio.openWritingPipe(address);
+    radio.setPALevel(RF24_PA_MIN);
+    radio.stopListening();
 }
 
-void loop() {
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
-  delay(1000);
+void loop()
+{
+    // const char text[] = "Hello World";
+    String mensaje;
+    int clave;
+
+    Serial.println("Ingrese el mensaje: ");
+    while (!Serial.available())
+    {
+        // Esperar hasta que se ingrese el mensaje en el monitor serial
+    }
+    mensaje = Serial.readString();
+
+    Serial.println("Ingrese la clave de cifrado: ");
+    while (!Serial.available())
+    {
+        // Esperar hasta que se ingrese la clave en el monitor serial
+    }
+    clave = Serial.parseInt();
+
+    String mensaje_cifrado = cifrar(mensaje, clave);
+
+    Serial.println("Mensaje cifrado: " + mensaje_cifrado);
+
+    radio.write(&text, sizeof(text));
+    delay(1000);
 }
--- cesar/recibir_cesar.ino
+++ cesar/recibir_cesar.ino
@@ -1,12 +1,36 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <Arduino.h>
+
+// Función para cifrar el mensaje usando el cifrado de César
+String cifrar(String mensaje, int clave) {
+    String mensaje_cifrado = "";
+
+    for (int i = 0; i < mensaje.length(); i++) {
+        if (isAlpha(mensaje.charAt(i))) {
+            char caracter = isUpperCase(mensaje.charAt(i)) ? 'A' : 'a';
+            mensaje_cifrado += (char)(((mensaje.charAt(i) - caracter + clave) % 26) + caracter);
+        } else {
+            mensaje_cifrado += mensaje.charAt(i);
+        }
+    }
+
+    return mensaje_cifrado;
+}
+
+// Función para descifrar el mensaje usando el cifrado de César
+String descifrar(String mensaje_cifrado, int clave) {
+    return cifrar(mensaje_cifrado, 26 - clave);
+}
+
 
 RF24 radio(7, 8); // CE, CSN
 
 const byte address[6] = "00001";
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   radio.begin();
   radio.openReadingPipe(0, address);
@@ -14,10 +38,26 @@ void setup() {
   radio.startListening();
 }
 
-void loop() {
-  if (radio.available()) {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
+void loop()
+{
+  if (radio.available())
+  {
+    // char text[32] = "";
+    // radio.read(&text, sizeof(text));
+
+    String mensaje;
+    radio.read(&mensaje, sizeof(mensaje));
+
+    int clave;
+    Serial.println("Ingrese la clave de cifrado: ");
+    while (!Serial.available())
+    {
+      // Esperar hasta que se ingrese la clave en el monitor serial
+    }
+    clave = Serial.parseInt();
+
+    String mensaje_descifrado = descifrar(mensaje, clave);
+
+    Serial.println("Mensaje descifrado: " + mensaje_descifrado);
   }
 }
