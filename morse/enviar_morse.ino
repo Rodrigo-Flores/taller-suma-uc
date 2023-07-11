96c9325d4c20882bc98b7d74227c2afbd326ab53
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2023-07-11 00:56:43 -0400

    receptor y transmisor para morse listo para probar

--- morse/enviar_morse.ino
+++ morse/enviar_morse.ino
@@ -1,20 +1,118 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <Arduino.h>
+#include <string.h>
+
+char characters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                     'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3',
+                     '4', '5', '6', '7', '8', '9', ' '};
+
+const char *morseCodes[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
+                            ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.",
+                            "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "-----",
+                            ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "/"};
+
+String cifrar(String mensaje)
+{
+  String mensaje_cifrado = "";
+
+  for (int i = 0; i < mensaje.length(); i++)
+  {
+    char caracter = toupper(mensaje.charAt(i));
+
+    if (caracter == ' ')
+    {
+      mensaje_cifrado += "/ ";
+    }
+    else
+    {
+      for (int j = 0; j < sizeof(characters); j++)
+      {
+        if (characters[j] == caracter)
+        {
+          mensaje_cifrado += morseCodes[j];
+          mensaje_cifrado += " ";
+          break;
+        }
+      }
+    }
+  }
+
+  mensaje_cifrado += "/";
+  return mensaje_cifrado;
+}
+
+String descifrar(String mensaje_cifrado)
+{
+  String mensaje_descifrado = "";
+  String morse = "";
+
+  for (int i = 0; i < mensaje_cifrado.length(); i++)
+  {
+    char caracter = mensaje_cifrado.charAt(i);
+
+    if (caracter != ' ')
+    {
+      morse += caracter;
+    }
+    else
+    {
+      if (morse == "/")
+      {
+        mensaje_descifrado += ' '; // Agregar espacio entre palabras
+      }
+      else
+      {
+        bool encontrado = false;
+        for (int j = 0; j < sizeof(morseCodes); j++)
+        {
+          if (strcmp(morseCodes[j], morse.c_str()) == 0)
+          {
+            mensaje_descifrado += characters[j];
+            encontrado = true;
+            break;
+          }
+        }
+        if (!encontrado)
+        {
+          mensaje_descifrado += 'e'; // Caracter desconocido
+        }
+      }
+      morse = "";
+    }
+  }
+
+  return mensaje_descifrado;
+}
 
 RF24 radio(7, 8); // CE, CSN
 
 const byte address[6] = "00001";
 
-void setup() {
+void setup()
+{
+  Serial.begin(9600);
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
   radio.stopListening();
 }
 
-void loop() {
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
+void loop()
+{
+  // const char text[] = "Hello World";
+
+  String mensaje;
+  Serial.println("Ingrese el mensaje: ");
+  while (!Serial.available())
+  {
+    // Esperar hasta que se ingrese el mensaje en el monitor serial
+  }
+  mensaje = Serial.readString();
+  mensaje_transformado = cifrar(mensaje);
+  // radio.write(&text, sizeof(text));
+  radio.write(&mensaje_transformado, sizeof(mensaje_transformado));
   delay(1000);
 }
--- morse/recibir_morse.ino
+++ morse/recibir_morse.ino
@@ -1,12 +1,98 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <Arduino.h>
+#include <string.h>
+
+char characters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                     'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3',
+                     '4', '5', '6', '7', '8', '9', ' '};
+
+const char *morseCodes[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
+                            ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.",
+                            "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "-----",
+                            ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "/"};
+
+String cifrar(String mensaje)
+{
+  String mensaje_cifrado = "";
+
+  for (int i = 0; i < mensaje.length(); i++)
+  {
+    char caracter = toupper(mensaje.charAt(i));
+
+    if (caracter == ' ')
+    {
+      mensaje_cifrado += "/ ";
+    }
+    else
+    {
+      for (int j = 0; j < sizeof(characters); j++)
+      {
+        if (characters[j] == caracter)
+        {
+          mensaje_cifrado += morseCodes[j];
+          mensaje_cifrado += " ";
+          break;
+        }
+      }
+    }
+  }
+
+  mensaje_cifrado += "/";
+  return mensaje_cifrado;
+}
+
+String descifrar(String mensaje_cifrado)
+{
+  String mensaje_descifrado = "";
+  String morse = "";
+
+  for (int i = 0; i < mensaje_cifrado.length(); i++)
+  {
+    char caracter = mensaje_cifrado.charAt(i);
+
+    if (caracter != ' ')
+    {
+      morse += caracter;
+    }
+    else
+    {
+      if (morse == "/")
+      {
+        mensaje_descifrado += ' '; // Agregar espacio entre palabras
+      }
+      else
+      {
+        bool encontrado = false;
+        for (int j = 0; j < sizeof(morseCodes); j++)
+        {
+          if (strcmp(morseCodes[j], morse.c_str()) == 0)
+          {
+            mensaje_descifrado += characters[j];
+            encontrado = true;
+            break;
+          }
+        }
+        if (!encontrado)
+        {
+          mensaje_descifrado += 'e'; // Caracter desconocido
+        }
+      }
+      morse = "";
+    }
+  }
+
+  return mensaje_descifrado;
+}
 
 RF24 radio(7, 8); // CE, CSN
 
 const byte address[6] = "00001";
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   radio.begin();
   radio.openReadingPipe(0, address);
@@ -14,10 +100,15 @@ void setup() {
   radio.startListening();
 }
 
-void loop() {
-  if (radio.available()) {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
+void loop()
+{
+  if (radio.available())
+  {
+    // char text[32] = "";
+    String mensaje;
+    mensaje_transformado = descifrar(mensaje);
+    // radio.read(&text, sizeof(text));
+    radio.read(&mensaje_transformado, sizeof(mensaje_transformado));
+    Serial.println(mensaje_transformado);
   }
 }
