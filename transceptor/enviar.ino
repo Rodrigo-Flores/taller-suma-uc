a61ef7bd60f01d4d14bd2402b3a6a48078d8e085
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2023-07-14 11:48:57 -0400

    puerto serial iniciado

--- transceptor/enviar.ino
+++ transceptor/enviar.ino
@@ -7,6 +7,7 @@ RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
 
 void setup() {
+  Serial.begin(9600);
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_MIN);
